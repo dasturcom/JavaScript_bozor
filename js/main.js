5eae591d040f751562f6863c2dc78460ddb9cbf9
dasturcom <abdujabborshavkatjonov@gmail.com>
2022-09-05 16:43:14 +0500

    JavaScript

--- js/main.js
+++ js/main.js
@@ -1,25 +1,50 @@
 var elShoppingForm = document.querySelector(".shopping-form");
 var elShoppingInput = document.querySelector(".shopping-input");
-var elShoppingBtn = document.querySelector(".shopping-btn");
 var elShoppingList = document.querySelector(".shopping-list");
 
 var products = [];
 
-elShoppingBtn.addEventListener("click", function(e) {
+elShoppingForm.addEventListener("submit", (e)=> {
     e.preventDefault();
     
-    products.push(elShoppingInput.value);
+    var addShoppingInput = elShoppingInput.value;
+
+    products.push(addShoppingInput);
     
-    elShoppingList.textContent = ""
+    elShoppingList.textContent = "";
     
     for (var product of products) {
         var liElement = document.createElement("li");
         liElement.textContent = product;
         
         elShoppingList.appendChild(liElement);
-        elShoppingInput.value = ""
+        elShoppingInput.value = "";
     }
     
 })
 
- 
+                                        // =============== CH E C K =============== //
+
+var elShoppingCheckForm = document.querySelector(".shopping-check-form");
+var elShoppingCheckInput = document.querySelector(".shopping-check-input");
+var elCheckResult = document.querySelector(".check-result");
+
+elShoppingCheckForm.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    
+    var checkInputValue =  elShoppingCheckInput.value;
+
+    var there = products.includes(checkInputValue);
+    
+    elShoppingList.textContent = "";
+    
+    if (there) {
+        elCheckResult.textContent = "B O R";
+    } else {
+        elCheckResult.textContent = "Y O' Q";
+    }
+
+    elShoppingCheckInput.value = "";    
+    elShoppingInput.focus();
+    
+})
